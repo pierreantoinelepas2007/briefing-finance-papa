e50eef9258d3a3dbc397e1e8f54b86254e6d365b
pierreantoinelepas2007 <lepaspierreantoine@gmail.com>
2026-06-12 17:06:25 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import os
  
 CONFIG = {
-    "GROQ_API_KEY":       "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",
+    "GROQ_API_KEY":       "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",
     "SENDER_EMAIL":       "pierreantoinelepas@gmail.com",
     "GMAIL_APP_PASSWORD": "uzrx istv viok ruby",
     "RECIPIENT_EMAIL":    "lepaspierreantoine@gmail.com",
